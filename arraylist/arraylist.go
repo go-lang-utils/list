d9731b440acb60c42b0a6ca414803e3c71ea140c
BPeters58 <benjamin.peters58@t-mobile.com>
2022-04-21 12:25:57 -0400

    array list refactor

---
+++ arraylist/arraylist.go
@@ -0,0 +1,59 @@
+package arraylist
+
+type arrayList[K any] struct {
+	slice *[]K
+}
+
+func New[K any](initValues ...K) arrayList[K] {
+	return arrayList[K]{
+		slice: &initValues,
+	}
+}
+
+func (l arrayList[K]) Add(item ...K) {
+	l.AddAll(item)
+}
+
+func (l arrayList[K]) Get(index int) K {
+	return (*l.slice)[index]
+}
+
+func (l arrayList[K]) Remove(index int) {
+	s := *l.slice
+	*l.slice = append(s[:index], s[index+1:]...)
+}
+
+func (l arrayList[K]) AddAll(anotherarrayList []K) {
+	*l.slice = append(*l.slice, anotherarrayList...)
+}
+
+func (l arrayList[K]) Size() int {
+	return len(*l.slice)
+}
+
+func (l arrayList[K]) isEmpty() bool {
+	return len(*l.slice) == 0
+}
+
+func (l arrayList[K]) ForEach(predicate func(index int, item K)) {
+	for i, k := range *l.slice {
+		predicate(i, k)
+	}
+}
+
+func (l arrayList[K]) Reverse() {
+	left := 0
+	right := len(*l.slice) - 1
+
+	for left < right {
+		tmp := (*l.slice)[left]
+		(*l.slice)[left] = (*l.slice)[right]
+		(*l.slice)[right] = tmp
+		left++
+		right--
+	}
+}
+
+func (l arrayList[K]) ToSlice() []K {
+	return *l.slice
+}
